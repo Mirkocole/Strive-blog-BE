9df52f45733fecbacadfd9eff21cfbe95a30ec22
Mirkocole <mirkocole92@gmail.com>
2024-05-11 16:08:04 +0200

    modifica cors22

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ server.use(express.json());
 
 const whitelist = ['https://mongo1-sigma.vercel.app/'];
 const optionsCors = {
-    origin : function(origin,callback) {
+    origin : function (origin,callback) {
         if (!origin || whitelist.some((domain)=> origin.startsWith(domain))) {
             callback(null,true);
         } else {
